ac8a990b7b0718a7fd7f7e99ee79c3616a224922
kevinwong197 <kevinwong1997@hotmail.com>
2018-07-18 07:23:24 -0400

    removed player click event

--- client/src/components/players/Player.vue
+++ client/src/components/players/Player.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row py-3 py-md-1" v-on:click="toClip()">
+  <div class="row py-3 py-md-1">
     <div class="name col-12 col-xs-8 col-md-8 col-lg-8">{{player.name}}</div>
     <div class="score col-6 col-xs-2 col-md-2 col-lg-2">{{player.score}}</div>
     <div class="time col-6 col-xs-2 col-md-2 col-lg-2">{{player.time}}</div>
@@ -10,40 +10,6 @@
 
 export default {
   props: ['player'],
-  methods: {
-    toClip () {
-      var id = 'clipboard'
-      var existsTextarea = document.getElementById(id)
-
-      if (!existsTextarea) {
-        var textarea = document.createElement('textarea')
-        textarea.id = id
-        textarea.style.position = 'fixed'
-        textarea.style.top = 0
-        textarea.style.left = 0
-        textarea.style.width = '1px'
-        textarea.style.height = '1px'
-        textarea.style.padding = 0
-        textarea.style.border = 'none'
-        textarea.style.outline = 'none'
-        textarea.style.boxShadow = 'none'
-        textarea.style.background = 'transparent'
-        document.querySelector('body').appendChild(textarea)
-        existsTextarea = document.getElementById(id)
-      }
-
-      existsTextarea.value = this.player.name
-      existsTextarea.select()
-
-      try {
-        document.execCommand('copy')
-      } catch (err) {
-        console.log('Unable to copy.')
-      }
-
-      alert(`Copied ${this.player.name} to clipboard`)
-    }
-  }
 }
 </script>
 
