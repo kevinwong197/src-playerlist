4a8c077fcfca4d15b4c08e72a73e61e8e8b58a06
kevinwong197 <kevinwong1997@hotmail.com>
2018-07-23 09:12:40 -0400

    removed comments in simplebar

--- client/src/components/SimpleBar.vue
+++ client/src/components/SimpleBar.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- Do not forget to add the dependency with npm install simplebar --save -->
   <div data-simplebar class="px-0">
     <div className="simplebar-scroll-content">
       <div className="simplebar-content">
