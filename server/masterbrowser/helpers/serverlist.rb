722776b7a1fa05abfa598dd20cfd1601908bec4f
kevinwong197 <kevinwong1997@hotmail.com>
2018-07-23 07:39:20 -0400

    refactored serverlist.rb

--- server/masterbrowser/helpers/serverlist.rb
+++ server/masterbrowser/helpers/serverlist.rb
@@ -3,38 +3,85 @@ require 'net/http'
 require 'uri'
 require 'pp'
 
+# encode: / => %5C
+# show dedicated server only: \\dedicated\\1
+# show non empty server only: \\empty\\1
 class Serverlist
-  def initialize mod
-    key = ENV['STEAM_API_KEY']
-    limit = 100
-    @url = 'https://api.steampowered.com'
-    @route = '/IGameServersService/GetServerList/v1/?' + URI.encode_www_form({
+  def initialize gamedir
+    @gamedir = gamedir
+  end
+
+  def querystr key: ENV['STEAM_API_KEY'], limit: 100
+    URI.encode_www_form({
       'key' => key,
-      # %5C
-      # \\dedicated\\1\\empty\\1
-      'filter' => "gamedir\\#{mod}",
+      'filter' => "gamedir\\#{@gamedir}",
       'limit' => limit
     })
   end
 
+  def apiurl
+    'https://api.steampowered.com'
+  end
+
+  def route
+    '/IGameServersService/GetServerList/v1/?'
+  end
+
+  def full_url
+    apiurl + api_call
+  end
+
+  def api_call
+    route + querystr
+  end
+
+  def print_content_type response
+    puts "content-type: #{response['content-type']}"
+  end
+
+  def hostname
+    URI.parse(full_url).host
+  end
+
+  def timeout_config
+    [open_timeout: 1, read_timeout: 1]
+  end
+
+  def ok? response
+    response.code == '200'
+  end
+
+  def clean_utf8 txt
+    txt.encode('UTF-8', 'UTF-8', invalid: :replace, undef: :replace)
+  end
+
+  def start &block
+    Net::HTTP.start(hostname, *timeout_config, &block)
+  end
+
+  def json2servers json
+    json['response']['servers'] || []
+  end
+
+  def fill_server_names servers
+    servers.each do |server|
+      server['name'] = server['name'] || 'unamed'
+    end
+  end
+
+  def read_servers txt
+    fill_server_names json2servers JSON.parse clean_utf8 txt
+  end
+
   def query
-    answer = nil
-    puts @url + @route
-    Net::HTTP.start(URI.parse(@url + @route).host, open_timeout: 1, read_timeout: 1) do |http|
-      resp = http.get(@route)
-      if resp.code == '200'
-        puts "content-type: #{resp['content-type']}"
-        json = JSON.parse resp.body.force_encoding('UTF-8').encode('UTF-8', invalid: :replace, undef: :replace)
-        data = json['response']['servers'] || []
-        data.each do |server|
-          server['name'] = server['name'] || 'unamed'
-        end
-        answer = {status: 'OK', servers: data}
-      else
-        answer = {status: "Error: #{resp.code} #{resp.message}"}
+    puts full_url
+    start do |http|
+      response = http.get(api_call)
+      if ok?(response)
+        return {status: 'OK', servers: read_servers(response.body)}
       end
     end
-    return answer
+    {status: "Error: #{resp.code} #{resp.message}"}
   rescue SocketError, Net::OpenTimeout, Net::ReadTimeout => e
     {status: "Error: #{e.message}"}
   end
