e87ea95a037c5c8ce15a8f6d2383f47c54774382
kevinwong197 <kevinwong1997@hotmail.com>
2018-07-31 00:43:46 -0400

    final card design

--- client/src/components/servers/Server.vue
+++ client/src/components/servers/Server.vue
@@ -1,63 +1,52 @@
 <template>
     <b-card
       class="nohighlight"
-      :class="selected ? 'dropped' : null"
       @click="select"
       :bg-variant="selected ? 'dark' : null">
-<!-- justify-content-md-left -->
 
+      <div class="row">
 
-
-
-      <div class="row mb-0">
-        <div class="col-6 col-md-auto d-md-none text-left">
-          <small
-            :class="selected ? 'text-white' : 'text-dark'"
-            class="w-100 bg-transparent text-truncate border-0">
-            {{server.map}}
-          </small>
-        </div>
-        <div class="col-6 d-md-none text-right">
-          <small
-            :class="selected ? 'text-white' : 'text-dark'"
-            class="w-100 bg-transparent border-0 text-white">
-            {{server.players}}/{{server.max_players}}
-          </small>
-        </div>
-
-
-        <div class="col-md-auto d-none d-md-flex cnt">
-          <b-input-group-text
-            :class="selected ? 'text-white' : 'text-dark'"
-            class="w-100 bg-transparent border-0 text-white cntdesktop">
-            {{server.players}}/{{server.max_players}}
-          </b-input-group-text>
-        </div>
-        <div class="col-md text-nowrap text-truncate">
-          <b-input-group-text
-            class="w-100 text-dark text-truncate">
+        <div class="col-md-12 text-left">
+          <h4
+            :class="selected ? 'selected' : 'unselected'">
             {{server.name}}
-          </b-input-group-text>
+          </h4>
         </div>
-        <div class="col-6 d-none d-md-flex col-md-auto">
-          <b-input-group-text
-            :class="selected ? 'text-white' : 'text-dark'"
-            class="w-100 bg-transparent text-truncate border-0">
-            {{server.map}}
-          </b-input-group-text>
+        <div class="col-md-auto d-none d-md-inline cntdesktop">
+          <b-badge
+            class="w-100"
+            :variant="selected ? 'light' : 'dark'">
+            {{server.players}}/{{server.max_players}}
+          </b-badge>
         </div>
-        <div class="col-md-auto d-none d-md-flex addr">
-          <b-btn class="w-100 bg-success text-white addrdesktop">
+        <div class="col-6 d-none d-md-flex col-md">
+          {{server.map}}
+        </div>
+        <div class="col-md-auto d-none d-md-inline addr" @click.stop>
+          <b-btn
+            class="w-100 addrdesktop py-0"
+            size="sm"
+            :href="steamproto(server.addr)"
+            :variant="server.dedicated ? 'primary' : 'secondary'"
+            :class="{disabled: !server.dedicated}">
             {{server.dedicated ? '↪' : ''}}{{server.addr}}
           </b-btn>
         </div>
 
+        <div class="col-6 d-md-none text-left">
+          {{server.map}}
+        </div>
+        <div class="col-6 d-md-none text-right">
+          <b-badge
+            :variant="selected ? 'light' : 'dark'">
+            {{server.players}}/{{server.max_players}}
+          </b-badge>
+        </div>
 
       </div>
 
-      <span @click.stop>
+      <span v-if="selected" @click.stop>
         <players
-          v-if="selected"
           :addr="server.addr"
           :parent="$el"
           class="mt-2"/>
@@ -122,20 +111,18 @@ export default {
 }
 .unselected {
   border-bottom-width: 2px;
+  border-color: #007bff;
   border-bottom-style: solid;
-}
-.dropped {
-  box-shadow: inset -2px -3px 78px -4px rgba(0,0,0,0.93);
 }
 .card-body {
   padding: 0.5rem;
 }
 .addrdesktop {
-  max-width: 13rem;
-  min-width: 13rem;
+  max-width: 11rem;
+  min-width: 11rem;
 }
 .cntdesktop {
-  max-width: 3rem;
-  min-width: 3rem;
+  max-width: 5rem;
+  min-width: 5rem;
 }
 </style>
